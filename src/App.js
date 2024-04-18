bcf993e6d8c12299daaa058e021768e41d41adce
kaursimar54 <166546484+kaursimar54@users.noreply.github.com>
2024-04-18 12:10:06 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="Testing framework">Welcome to React</h1>
+          <h1 className="Testing framework">Testing framework</h1>
         </header>
         <p className="hello">
           To get started, edit <code>src/App.js</code> and save to reload.
